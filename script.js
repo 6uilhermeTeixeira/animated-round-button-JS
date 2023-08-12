c75215607b0bdef21f9693515ea5cd590c7c02a1
6uilhermeTeixeira <tx.opus@outlook.com>
2023-08-12 20:01:19 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var body = document.querySelector("body");
+  var centerX = window.innerWidth / 1;
+  var centerY = window.innerHeight / 1;
+  var sensitivity = 0.05; // Ajuste a sensibilidade conforme necessário
+
+document.addEventListener("mousemove", function(event) {
+  var mouseX = event.clientX;
+  var mouseY = event.clientY;
+  
+  var offsetX = (mouseX - centerX) * sensitivity;
+  var offsetY = (mouseY - centerY) * sensitivity;
+    
+    // Verifica se o cursor está dentro da janela do navegador
+    if (mouseX >= 0 && mouseX <= window.innerWidth && mouseY >= 0 && mouseY <= window.innerHeight) {
+      // Atualiza as propriedades CSS do body para mover o background
+      body.style.backgroundPositionX = offsetX + "px";
+      body.style.backgroundPositionY = offsetY + "px";
+    }
+  });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.button');
+  button.classList.add('fade-in');
+});
